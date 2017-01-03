68da6df070c40f8eb34174e0d44282f0e5342713
Mohamed <mohamed@Mohameds-MacBook-Pro.local>
2017-01-03 13:41:48 +0200

    webpack, add build configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,5 @@
-const path = require('path');
-module.exports = {
-  context: path.join(__dirname, 'src'),
-  entry: {
-    popup: './popup.js',
-    background: './background.js',
-    worker: './worker.js'
-  },
-  output: {
-    path: __dirname + '/app',
-    filename: '[name].js'
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel',
-        query: {
-          presets: ['stage-3', 'stage-0', 'es2017', 'react', 'es2017', 'es2015'],
-          plugins: ['transform-runtime']
-        }
-      }, {
-        test: /\.(sass|scss)$/,
-        loaders: ['style', 'css', 'sass']
-      },
-      {
-        test: /\.css$/,
-        loaders: ['style', 'css']
-      },
-      {
-        test: /\.(woff2?|eot|ttf|svg)$/,
-        loaders: ['url']
-      }
-    ]
-  }
-};
+if(process.env.NODE_ENV === 'production') {
+  module.exports = require('./webpack.config-production');
+} else {
+  module.exports = require('./webpack.config-dev');
+}
